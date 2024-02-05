e1732403a63c972dc8a94183dfc3b99ea0b0be4c
da-gemini <56649068+da-gemini@users.noreply.github.com>
2024-02-04 23:36:04 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,13 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fullName = $_POST['fullName'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $to = "arch.jovial@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission";
+    $message = "Name: " . $_POST["fullName"] . "\n";
+    $message .= "Email: " . $_POST["email"] . "\n";
+    $message .= "Message: " . $_POST["message"];
 
-    $to = 'arch.jovial@gmail.com'; // Replace with your email address
-    $subject = 'New Form Submission';
-    $headers = "From: $fullName <$email>";
+    // Additional headers
+    $headers = "From: " . $_POST["email"];
 
+    // Send email
     mail($to, $subject, $message, $headers);
 }
 ?>
